a981d58fe71062be530850bebc3e8fa39bc130b3
yaowenqiang <yaowenqiang111@163.com>
2021-05-04 17:01:10 +0800

    init commit

---
+++ NOTES.md
@@ -0,0 +1,8 @@
+> mix new servy
+> elixir lib/servy.ex  #run the code
+> iex > String.reverse("peek")
+> iex > c "lib/servy.ex"
+> iex lib/servy.ex
+> iex -S mix
+> iex(1)> Servy.hello("world")
+> iex > r Servy # recompile
---
+++ lib/servy.ex
@@ -0,0 +1,21 @@
+defmodule Servy do
+  @moduledoc """
+  Documentation for `Servy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Servy.hello()
+      :world
+
+  """
+  def hello(name) do
+    "Hello, #{name}!"
+  end
+end
+
+IO.puts Servy.hello("Elixir")
+
