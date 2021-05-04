beb89712d9a18c53d574d59615096aea1f8edeb5
yaowenqiang <yaowenqiang111@163.com>
2021-05-04 19:01:38 +0800

    add route pattern match

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -18,8 +18,14 @@ defmodule Servy.Handler do
       |> String.split("\n") 
       |> List.first
       |> String.split(" ")
-    %{ method: method, path: path, resp_body: ""}
+
+    %{ method: method, 
+      path: path, 
+      resp_body: "",
+      status: nil
+    }
   end
+
 
   def route(conv) do
     #conv[:method]
@@ -30,16 +36,24 @@ defmodule Servy.Handler do
   end
 
   def route(conv,"GET",  "/wildthings") do
-    %{conv | resp_body: "Bears, Lions, Tigers" }
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(conv,"GET", "/bears") do
-    %{conv | resp_body: "Teddy, Smokey, Paddington" }
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  end
+  #pattern match /bears/1
+  def route(conv,"GET", "/bears/" <> id) do
+    %{conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(conv, _method, path) do
+   %{conv | status: 404, resp_body: "No #{path} here!" } 
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 ok
+    HTTP/1.1 #{conv.status} #{status_reson(conv.status)}
     Content-type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
@@ -47,6 +61,19 @@ defmodule Servy.Handler do
     """
 
   end
+
+  #private function
+  defp status_reson(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error",
+    }[code]
+  end
+
 end
 
 request = """
@@ -83,6 +110,17 @@ IO.puts response
 
 request = """
 GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
