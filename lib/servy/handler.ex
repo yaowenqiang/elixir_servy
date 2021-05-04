334abc23fda390c12af61ffa3ca7069d0e019832
yaowenqiang <yaowenqiang111@163.com>
2021-05-04 17:24:25 +0800

    add first request demo

---
+++ lib/servy/handler.ex
@@ -0,0 +1,49 @@
+defmodule Servy.Handler do
+  def handle(request) do
+#conv = parse(request)    
+#conv = route(conv)
+#format_response(conv)
+    request 
+    |> parse 
+    |> route 
+    |> format_response
+  end
+  def parse(request) do
+    conv = %{ method: "GET", path: "/wildthings", resp_body: ""}
+  end
+
+  def route(conv) do
+    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 ok
+    Content-type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+expected_response = """
+HTTP/1.1 200 ok
+Content-type: text/html
+Content-Length: 20
+
+Bears, Lions, Tigers
+"""
+
+
+response = Servy.Handler.handle(request)
+IO.puts response
