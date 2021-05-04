3114d46744d50ea17a5d14f45a5c3822e6ff9a63
yaowenqiang <yaowenqiang111@163.com>
2021-05-04 19:41:56 +0800

    rewrite path and track 404

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -5,10 +5,25 @@ defmodule Servy.Handler do
 #format_response(conv)
     request 
     |> parse 
+    |> rewrite_path 
     |> log 
     |> route 
+    |> track 
     |> format_response
   end
+
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} is no the loose!"
+    conv
+  end
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings" } 
+  end
+
+  def rewrite_path(conv), do: conv
 
   def log(conv) , do:  IO.inspect conv
 
@@ -27,27 +42,40 @@ defmodule Servy.Handler do
   end
 
 
-  def route(conv) do
+#def route(conv) do
     #conv[:method]
     #conv.method
     #conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
     #%{conv | resp_body: "Bears, Lions, Tigers" }
-    route(conv, conv.method, conv.path)
-  end
+    #route(conv, conv.method, conv.path)
+#  end
 
-  def route(conv,"GET",  "/wildthings") do
+#def route(%{method: "GET", path: "/wildthings"}) do
+
+#end
+
+
+#def route(conv,"GET",  "/wildthings") do
+  def route(%{ method: "GET",path: "/wildthings" } = conv ) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(conv,"GET", "/bears") do
+#def route(conv,"GET", "/bears") do
+  def route(%{method: "GET",path: "/bears" } = conv ) do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
   #pattern match /bears/1
-  def route(conv,"GET", "/bears/" <> id) do
+#def route(conv,"GET", "/bears/" <> id) do
+  def route(%{method: "GET",path: "/bears" <> id } = conv ) do
     %{conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(conv, _method, path) do
+
+#def route(conv, _method, path) do
+#   %{conv | status: 404, resp_body: "No #{path} here!" } 
+#  end
+
+def route(%{path: path} = conv) do
    %{conv | status: 404, resp_body: "No #{path} here!" } 
   end
 
@@ -121,6 +149,17 @@ IO.puts response
 
 request = """
 GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
